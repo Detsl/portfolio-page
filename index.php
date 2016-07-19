8840588e9bf7c9093dc1b5d1efc3aea3f60bf719
detsl <denistsarkov@gmail.com>
2016-07-19 10:59:01 +0300

    made first portfolio page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<header>
+</header>
+</body>
+</html>
+
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ghettogeek
+ * Date: 17.07.16
+ * Time: 8:36
+ */
